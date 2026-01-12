f3447161bf4f11e75434d1a663d0b7f790c6f89b
sahrul <sahrul.devspace@gmail.com>
2026-01-13 00:24:19 +0700

    first commit

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db } from 'mongodb'
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://user:pass@localhost:27017/'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://sahruldevspace_db_user:R0EsBAmvqSb4PWkZ@cluster0.qguxvga.mongodb.net/'
 const MONGODB_DB = process.env.MONGODB_DB || 'stockbit'
 
 let cachedClient: MongoClient | null = null
